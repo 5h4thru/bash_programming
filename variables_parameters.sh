078b10249ae7267b55fbff26177792eeafea6af4
root <root@localhost.localdomain>
2016-06-29 21:48:45 -0400

    Completed part 4 of variables_parameters.sh

--- variables_parameters.sh
+++ variables_parameters.sh
@@ -60,8 +60,70 @@ echo "Value of num is $num"
 var=${num/10/b}
 echo "Value of var after replacing 10 with letter b in num and assigning it to var: $var"
 
+# $(command) is "command substitution".  As you seem to understand, it runs the command, captures its output, and inserts that into the command line that contains the $(…)
+# ${parameter} is "parameter substitution".  A lot of information can be found in the shell’s man page
+
+
 #########################
 # Part 4
 #########################
 echo -e "\n##### Part 4 #####"
+# -n tests if the parameter is specified
+
+if [ -n "$1" ]
+then
+  echo "1st argument is $1"
+fi
+
+if [[ -n "$2" ]]
+then
+  echo "2nd argument is $2"
+fi
+
+if [[ -n "$3" ]]
+then
+  echo "3rd argument is $3"
+fi
+
+if [[ -n "$4" ]]
+then
+  echo "4th argument is $4"
+fi
+
+if [[ -n "$5" ]]
+then
+  echo "5th argument is $5"
+fi
+
+if [[ -n "$6" ]]
+then
+  echo "6th argument is $6"
+fi
+
+if [[ -n "$7" ]]
+then
+  echo "7th argument is $7"
+fi
+
+if [[ -n "$8" ]]
+then
+  echo "8th argument is $8"
+fi
+
+if [[ -n "$9" ]]
+then
+  echo "9th argument is $9"
+fi
+
+if [ -n "${10}" ]
+then
+  echo "10th argument is ${10}"
+fi
+
+echo "List of arguments: "$*""
+echo "Name of the script is \""$0"\""
+if [[ "$#" -lt 10 ]]
+then
+  echo "Require 10 arguments to run the part4 properly"
+fi
 
